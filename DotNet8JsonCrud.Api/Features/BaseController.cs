99e6560459cc5d1bfb6948a7883173d5de18a146
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-21 00:25:50 +0630

    Format BaseController

--- DotNet8JsonCrud.Api/Features/BaseController.cs
+++ DotNet8JsonCrud.Api/Features/BaseController.cs
@@ -1,15 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
+﻿namespace DotNet8JsonCrud.Api.Features;
 
-namespace DotNet8JsonCrud.Api.Features
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(obj.Serialize(), "application/json");
-        }
+        return Content(obj.Serialize(), "application/json");
     }
 }
