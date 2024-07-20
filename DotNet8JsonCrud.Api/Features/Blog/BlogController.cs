00f5aa6783bf5f549b705c578643caa2742af7f5
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-21 00:25:02 +0630

    Format Blog Controller

--- DotNet8JsonCrud.Api/Features/Blog/BlogController.cs
+++ DotNet8JsonCrud.Api/Features/Blog/BlogController.cs
@@ -1,52 +1,48 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿namespace DotNet8JsonCrud.Api.Features.Blog;
 
-namespace DotNet8JsonCrud.Api.Features.Blog
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly BL_Blog _bL_Blog;
+
+    public BlogController(BL_Blog bL_Blog)
     {
-        private readonly BL_Blog _bL_Blog;
+        _bL_Blog = bL_Blog;
+    }
 
-        public BlogController(BL_Blog bL_Blog)
-        {
-            _bL_Blog = bL_Blog;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs()
+    {
+        var result = await _bL_Blog.GetBlogs();
+        return Content(result);
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetBlogs()
-        {
-            var result = await _bL_Blog.GetBlogs();
-            return Content(result);
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreateBlog([FromBody] BlogModel blog)
+    {
+        var result = await _bL_Blog.CreateBlogV1(blog);
+        return Content(result);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateBlog([FromBody] BlogModel blog)
-        {
-            var result = await _bL_Blog.CreateBlogV1(blog);
-            return Content(result);
-        }
+    [HttpPut("{blogId}")]
+    public async Task<IActionResult> UpdateBlog([FromBody] BlogModel blog, string blogId)
+    {
+        var result = await _bL_Blog.UpdateBlogV1(blog, blogId);
+        return Content(result);
+    }
 
-        [HttpPut("{blogId}")]
-        public async Task<IActionResult> UpdateBlog([FromBody] BlogModel blog, string blogId)
-        {
-            var result = await _bL_Blog.UpdateBlogV1(blog, blogId);
-            return Content(result);
-        }
+    [HttpPatch("{blogId}")]
+    public async Task<IActionResult> PatchBlog([FromBody] BlogModel blog, string blogId)
+    {
+        var result = await _bL_Blog.PatchBlogV1(blog, blogId);
+        return Content(result);
+    }
 
-        [HttpPatch("{blogId}")]
-        public async Task<IActionResult> PatchBlog([FromBody] BlogModel blog, string blogId)
-        {
-            var result = await _bL_Blog.PatchBlogV1(blog, blogId);
-            return Content(result);
-        }
-
-        [HttpDelete("{blogId}")]
-        public async Task<IActionResult> DeleteBlog(string blogId)
-        {
-            var result = await _bL_Blog.DeleteBlogV1(blogId);
-            return Content(result);
-        }
+    [HttpDelete("{blogId}")]
+    public async Task<IActionResult> DeleteBlog(string blogId)
+    {
+        var result = await _bL_Blog.DeleteBlogV1(blogId);
+        return Content(result);
     }
 }
--- DotNet8JsonCrud.Api/GlobalUsings.cs
+++ DotNet8JsonCrud.Api/GlobalUsings.cs
@@ -4,3 +4,4 @@ global using DotNet8JsonCrud.Api.Enums;
 global using DotNet8JsonCrud.Api.Helpers;
 global using DotNet8JsonCrud.Api.Models;
 global using DotNet8JsonCrud.Api.Resources;
+global using Microsoft.AspNetCore.Mvc;
