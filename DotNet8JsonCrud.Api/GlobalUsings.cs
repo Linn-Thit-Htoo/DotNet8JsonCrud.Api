3af035ee098079f4570f614031a866c3b6f3cd45
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-21 00:24:22 +0630

    [Added] Global Usings

---
+++ DotNet8JsonCrud.Api/GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Global using directives
+
+global using DotNet8JsonCrud.Api.Enums;
+global using DotNet8JsonCrud.Api.Helpers;
+global using DotNet8JsonCrud.Api.Models;
+global using DotNet8JsonCrud.Api.Resources;
